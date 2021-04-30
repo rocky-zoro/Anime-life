1c139f2b397093a09e926da89ac6aff2d0d09f32
Rocky <mehrarijul2@gmail.com>
2021-05-01 00:16:12 +0530

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const mongoose = require('mongoose');
 module.exports = {
-    name:'okaymonsterdeletethatstuffbecauseithasalotofduplicatesIsuggestusethatcommandmaybeitwillworkidk',
+    name:'okaymonsterdeletethatstuffbecauseithasalotofduplicates',
     desciption:"check your watchlist",
 
     
