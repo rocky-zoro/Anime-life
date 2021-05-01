3e7b2f97338aa8a689f3a1219b72a0c23a406bc3
Rocky <mehrarijul2@gmail.com>
2021-05-01 21:41:04 +0530

    klqe

--- commands/roleplay.js
+++ commands/roleplay.js
@@ -22,7 +22,7 @@ module.exports = {
             .setDescription('roleplay')
             .setColor('RANDOM')
             .setURL('https://discord.gg/adnga86cdA')
-            .setImage(`${json.results[index].url}`)
+            .setImage(`${json.results[index].itemurl}`)
             .setFooter('For more info use ?search,?dbf or ?moreinfo','https://imgur.com/22ncPbk.png')
             message.channel.send(json.results[index].url)
             
