abd613f01f7ee4c2daa89d03edccee67f4882469
Rocky <mehrarijul2@gmail.com>
2021-05-03 19:56:06 +0530

    ds

--- commands/roleplay.js
+++ commands/roleplay.js
@@ -6,70 +6,98 @@ module.exports = {
     description:'roleplay',
     async execute(client, message, args, Discord){
 
+        try {
         
-        let note = args.join(" ");
-        if(!note) return message.channel.send("You forgot to include the title of the Manga, mate.\nTo see how to remove a manga from your manga list, look at the example.\nE.g.-`?rm Kawaii`, `?roleplay Pat`");
+        let note = args.join(" ").trim().split(" ",2);
+        if(!note) return message.channel.send("You forgot to include the title of the Manga, mate.\nTo see how to remove a manga from your manga list, look at the example.\nE.g.-`?rp Kawaii`, `?roleplay Pat`");
+        if(!note[1]){
+            taggeruser = message.author.username;
+        }
+        if(note[1].startsWith('<@') && note[1].endsWith('>')) {
+            mention = note[1].slice(2,-1)
+            if(mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+            taggeruse = await client.users.fetch(mention)
+            taggeruser = taggeruse.username;
+        } else if (note[1].match(/(\d+)/)) {
+            taggeruse = await client.users.fetch(wo[1]);
+            taggeruser = taggeruse.username;
+        } else {
+            taggeruser = note[1];
+        }
         let st;
-        switch(note){
+        let doing;
+        switch(note[0]){
             case 'cute':
                 st = "anime hug";
                 limit = 10;
                 break;
 
-            case'tch':
+            case 'tch':
                 st = 'tch';
+                doing = ["is annoyed by","is displeased by"];
                 limit = 4;
                 break;
         
-            case'laugh':
+            case 'laugh':
                 st = 'anime laughing';
                 limit = 15;
+                doing = "is teasing";
                 break;
-            case'shy':
-                st = '20';
-                limit = 2;
+            case 'shy':
+                st = 'anime shy';
+                limit = 20;
                 break;
-            case'yes':
+            case 'yes':
                 st = 'anime yes';
                 limit = 8;
                 break;
-            case'pat':
+            case 'pat':
                 st = 'anime pat';
                 limit = 10;
                 break;
-            case'kick':
+            case 'kick':
                 st = 'anime kick';
                 limit = 10;
                 break;
-            case'punch':
-                st = 'anime puncg';
+            case 'punch':
+                st = 'anime punch';
                 limit = 10;
                 break;
-            case'slap':
+            case 'slap':
                 st = 'anime slap';
                 limit = 10;
                 break;
-            case'good night':
-                st = 'anime good night';
+            case 'night':
+                st = 'anime good night'; //cry, uwu, died, sorry,sleep,defeat, no bully, finger, boo, smirk, think, clap, swag
                 limit = 10;
                 break;
-            case'hehe':
+            case 'hehe':
                 st = 'anime hehe';
                 limit = 10;
                 break;
-            case'okay':
+            case 'okay':
                 st = 'anime okay';
                 limit = 2;
                 break;
-                
+            case 'cry':
+                st = 'anime cry';
+                limit = 30;
+                break;
+            case 'uwu':
+                st = 'anime love'
+                limit = 15;
+                break;
+            
             default:
-                    
-                st = "anime no";
+                     
+                st = "anime nope";
                 limit = 50;
 
         }
+        doing = math.floor(math.random()*doing.length); 
         let url = `https://g.tenor.com/v1/search?q=${st}&key=${process.env.tenorkey}&limit=${limit}`;
-        try {
             let response = await fetch(url);
             let json = await response.json();
             console.log(json.results);
@@ -77,12 +105,12 @@ module.exports = {
         
             const newEmbed = await new Discord.MessageEmbed()
             
-            .setTitle(`**${note}**`)
+            .setTitle(`**${message.author.user}${doing}${taggeruser}**`)
             .setDescription('roleplay')
             .setColor('RANDOM')
             .setURL('https://discord.gg/adnga86cdA')
             .setImage(`${json.results[index].media[0].gif.url}`)
-            .setFooter('For more info use ?search,?dbf or ?moreinfo','https://imgur.com/22ncPbk.png')
+            .setFooter('Support us','https://imgur.com/22ncPbk.png')
             
             message.channel.send(newEmbed);
 
