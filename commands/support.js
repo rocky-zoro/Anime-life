8b1c26441ea597c05f7301bd811becba922d2e84
Rocky <mehrarijul2@gmail.com>
2021-05-21 15:12:43 +0530

    hj

--- commands/support.js
+++ commands/support.js
@@ -6,9 +6,9 @@ module.exports = {
     execute(client, message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setTitle("Support us on Pateron","patreon.com/anili")
-        .setDescription("I want to create a game engine called Anili Engine, and I need your help to do so. You can find more details about Pateron benefits here.\npatreon.com/anili")
+        .setDescription("I want to create a game engine called Anili Engine, and I need your help to do so. You can find more details about Pateron benefits here.\n patreon.com/anili")
         .setFooter("Support us")
-        .setImage("https://imgur.com/sVe7Zac")
+        .setImage("https://imgur.com/sVe7Zac.jpg")
         message.channel.send(newEmbed);
     }
 }
