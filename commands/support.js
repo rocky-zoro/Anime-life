7ae98bcf49474d7d506de7c92273a80a7a844536
Rocky <mehrarijul2@gmail.com>
2021-05-21 15:28:32 +0530

    ds

--- commands/support.js
+++ commands/support.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(client, message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setTitle("Support us on Pateron","patreon.com/anili")
-        .setDescription("I want to create a game engine called Anili Engine, and I need your help to do so. You can find more details about Pateron benefits here.\n patreon.com/anili")
+        .setDescription("I want to create a game engine called Anili Engine, and I need your help to do so. You can find more details about Pateron benefits here.\n https://patreon.com/anili")
         .setFooter("Support us")
         .setImage("https://imgur.com/sVe7Zac.jpg")
         message.channel.send(newEmbed);
--- commands/votes.js
+++ commands/votes.js
@@ -4,18 +4,6 @@ module.exports = {
 
     name:'vote',
     execute(client, message, args, Discord){
-        const newEmbed = new Discord.MessageEmbed()
-        .setTitle("Vote Support")
-        .setDescription("Support us on these two sites by clicking on the link.")
-        .addFields(
-            {
-            name: "Discord Bot List",
-            value:"discord.ly/anime-life"
-        
-            }         
-
-        )
-        .setFooter("Support us")
-        message.channel.send(newEmbed);
+        message.channel.send("**Vote Support**\nSupport us on these two sites by clicking on the link\n\n **Discord Bot List**\nhttps://discord.ly/anime-life")
     }
 }
