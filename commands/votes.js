587749e8928a5e1571b71222943f9f24c6a66fc2
Rocky <mehrarijul2@gmail.com>
2021-05-21 15:08:47 +0530

    sa

--- commands/votes.js
+++ commands/votes.js
@@ -2,17 +2,20 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
 
-    name:'support',
+    name:'vote',
     execute(client, message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setTitle("Vote Support")
         .setDescription("Support us on these two sites by clicking on the link.")
         .addFields(
-            
+            {
+            name: "Discord Bot List",
+            value:"discord.ly/anime-life"
+        
+            }         
 
         )
         .setFooter("Support us")
-        .setImage("https://imgur.com/sVe7Zac")
         message.channel.send(newEmbed);
     }
 }
